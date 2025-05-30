c1ac66aa3aab27ae0ef22a2841c31bc2a78058c2
minayev_aleksandr <minayev_aleksandr@pro-compliance.ru>
2025-05-30 15:15:43 +0300

    Added docker compose. Inited migration

---
+++ Server/NoruBanner/NoruBanner.WebApi/Migrations/20250530120844_InitialCreate.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NoruBanner.WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BannerEvents",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    BannerId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserSessionId = table.Column<string>(type: "text", nullable: false),
+                    EventType = table.Column<int>(type: "integer", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    AdditionalData = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BannerEvents", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Banners",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ImageUrl = table.Column<string>(type: "text", nullable: false),
+                    SiteId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Banners", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BannerEvents_BannerId",
+                table: "BannerEvents",
+                column: "BannerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BannerEvents_UserSessionId",
+                table: "BannerEvents",
+                column: "UserSessionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Banners_SiteId",
+                table: "Banners",
+                column: "SiteId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BannerEvents");
+
+            migrationBuilder.DropTable(
+                name: "Banners");
+        }
+    }
+}
